f144abd3a65097320c77e0d8763766b3a6e0f7e2
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-08-03 17:03:05 +0900

    ddit

--- dbprogramming/src/dbprogramming/DBTestExample.java
+++ dbprogramming/src/dbprogramming/DBTestExample.java
@@ -15,6 +15,7 @@ public class DBTestExample {
 		//작업 번호 선택을 위한 scanner 사용
 		String sql=null;
 		//마지막에 쿼리문을 문자열로 담기위해 String 선언
+		List<Object> param=null;//param 변수 선언
 		
 		//CRUD - 생성,삭제,삽입,갱신
 		System.out.println("[[데이터베이스 관리]]");
@@ -52,22 +53,22 @@ public class DBTestExample {
 			System.out.println("======조회완료======");
 			break;
 		case 3: //신규입력(insert into~) 
-			List<Object> param=new ArrayList<Object>();
+			param=new ArrayList<Object>();
 			
-			System.out.println("상품코드 : ");
-			String p=sc.next();
+			System.out.print("상품코드 : ");
+			String p=sc.nextLine();
 			param.add(p);
 			
-			System.out.println("상품명 : ");
-			String pname=sc.next();
+			System.out.print("상품명 : ");
+			String pname=sc.nextLine();
 			param.add(pname);
 			
-			System.out.println("가격 : ");
-			int pr=sc.nextInt();
+			System.out.print("가격 : ");
+			int pr=Integer.parseInt(sc.next());
 			param.add(pr);
 			
-			System.out.println("적정재고 : ");
-			int ps=sc.nextInt();
+			System.out.print("적정재고 : ");
+			int ps=Integer.parseInt(sc.next());
 			param.add(ps);
 			
 			sql="INSERT INTO TPROD VALUES( ?, ?, ?, ? )";
@@ -78,8 +79,55 @@ public class DBTestExample {
 				System.out.println("자료가 입력되지 않았습니다.");
 			}
 			break;
-						
+		case 4:
+			param=new ArrayList<Object>();
+			sql="UPDATE TPROD SET ";
+			String yesOrNot="";
+			flag=0;
+			
+			System.out.print("변경할 상품번호 : ");
+			String cpid=sc.nextLine();
+			param.add(cpid);
+			
+			System.out.print("상품명 변경? (Y/N)");
+			yesOrNot = sc.nextLine();
+			while(yesOrNot.equalsIgnoreCase("y")) {
+				System.out.print("상품명 : ");
+				String p1=sc.nextLine();
+				sql=sql+"PROD_NAME = '"+p1+"', ";
+				break;
+			}
+			System.out.print("상품가격 변경? (Y/N)");
+			yesOrNot = sc.nextLine();
+			while(yesOrNot.equalsIgnoreCase("y")) {
+				System.out.print("상품가격 : ");
+				int p2=Integer.parseInt(sc.nextLine());
+				sql=sql+"PROD_PRICE = "+p2+", ";	
+				break;
+			}
+			System.out.print("적정재고 변경? (Y/N)");
+			yesOrNot = sc.nextLine();
+			while(yesOrNot.equalsIgnoreCase("y")) {
+				System.out.print("적정재고 : ");
+				int p3=Integer.parseInt(sc.nextLine());
+				sql=sql+"PROD_PROPERSTOCK = "+p3+", ";
+				break;
+			}
+			
+			int len = sql.length();
+			sql=sql.substring(0,len-2);
+			sql=sql+" WHERE PROD_ID = ? ";
+			
+			flag=dt.update(sql, param);
+			if(flag != 0) {
+				System.out.println("자료갱신 성공");
+			}else {
+				System.out.println("자료갱신 실패");				
+			}
+			break;
+			
 		}
 			
 	}
+	
 }
