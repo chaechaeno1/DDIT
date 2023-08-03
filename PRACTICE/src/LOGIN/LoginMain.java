4097d6a88b1c952b1ff0054901f77929bf878c22
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-08-03 11:54:26 +0900

    login practice

---
+++ PRACTICE/src/LOGIN/LoginMain.java
@@ -0,0 +1,31 @@
+package LOGIN;
+
+public class LoginMain {
+	// private : 자기 자신의 클래스 내에서만 접근 가능
+	private String id;
+	private String password;
+	// static : 모든 객체가 공유하여 하나의 멤버를 어디서든지 참조할 수 있지만,
+	// 			static 영역 멤버들은 프로그램 종ㅇ료시까지 메모리가 할당된 채로 존재
+	// static 선언 기준 : 공용으로 사용할 것인지 아닌지
+	static 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
